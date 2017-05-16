4e46072f195ac506b1b4da34c79c687e9235e364
BrianYi <yijiazhen123@gmail.com>
2017-05-16 15:21:49 +0800

    add qss file

--- TinyFTP/main.cpp
+++ TinyFTP/main.cpp
@@ -3,13 +3,18 @@
 
 int main(int argc, char *argv[])
 {
-	QApplication a(argc, argv);
+	QApplication app(argc, argv);
 
     QTextCodec::setCodecForCStrings(QTextCodec::codecForName("gbk"));
     QTextCodec::setCodecForLocale(QTextCodec::codecForName("gbk"));
     QTextCodec::setCodecForTr(QTextCodec::codecForName("gbk"));
 
+	QFile file(":/tinyftp.qss");
+	if (!file.open(QFile::ReadOnly))
+		qDebug() << file.errorString() << endl;
+	app.setStyleSheet(file.readAll());
+
 	TinyFTP w;
 	w.show();
-	return a.exec();
+	return app.exec();
 }
